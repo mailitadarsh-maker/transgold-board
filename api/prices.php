e3b3ff5079b7000f1ceff44da69c08566abcb7e3
mailitadarsh-maker <mailitadarsh@gmail.com>
2026-06-28 12:28:55 +0530

    Implement price fetching and response in prices.php
    Fetch prices from a remote JSON file and include spread data.

---
+++ api/prices.php
@@ -0,0 +1,36 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Cache-Control: no-store');
+
+$url = 'https://raw.githubusercontent.com/mailitadarsh-maker/transgold-board/main/prices.json';
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$data = curl_exec($ch);
+$err  = curl_error($ch);
+curl_close($ch);
+
+if ($data && !$err) {
+    $json = json_decode($data, true);
+
+    $spreadFile = __DIR__ . '/../spread.json';
+    if (file_exists($spreadFile)) {
+        $spread = json_decode(file_get_contents($spreadFile), true);
+        $master = $spread['master'] ?? $spread;
+        $json['spread'] = [
+            'goldBid'   => $master['goldBid']   ?? 0,
+            'goldAsk'   => $master['goldAsk']   ?? 0,
+            'silverBid' => $master['silverBid'] ?? 0,
+            'silverAsk' => $master['silverAsk'] ?? 0,
+        ];
+    } else {
+        $json['spread'] = ['goldBid'=>0,'goldAsk'=>0,'silverBid'=>0,'silverAsk'=>0];
+    }
+
+    echo json_encode($json);
+} else {
+    echo json_encode(['ok' => false, 'error' => $err]);
+}
